91ca4356675fb829d343cb2cbf652d715eccca08
Gustavo Saez Ferreira <hero_uzumaki@hotmail.com>
2022-01-26 16:58:56 -0300

    Update App.js

--- clock/src/App.js
+++ clock/src/App.js
@@ -1,24 +1,15 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+export default function Exemplo() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
     </div>
   );
 }
-
-export default App;
