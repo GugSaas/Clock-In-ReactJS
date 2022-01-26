a170bc65ae91d1781729956714ce2d39176a16b4
Gustavo Saez Ferreira <hero_uzumaki@hotmail.com>
2022-01-26 17:06:35 -0300

    Update

--- clock/src/App.js
+++ clock/src/App.js
@@ -1,15 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React, {useEffect } from 'react';
 
-export default function Exemplo() {
-  const [count, setCount] = useState(0);
+export default function Clock() {
+  const [hora, setHora] = setState(5);
+  const [minuto, setMinuto] = setState(10);
 
   useEffect(() => {
-
+    const interval = setInterval(()=>{
+      setMinuto(minuto + 1)
+      if(minuto == 60){
+        setMinuto(0);
+        setHora(hora+1);
+      }
+    }, 1000);
+    return () => clearInterval(interval);
   });
 
   return (
     <div>
-      
+      <h1>{hora}:{minuto}</h1>
+      <hr></hr>
     </div>
   );
 }
